d91db641f2a6c26ff2b2ad60d2850f4826f4e785
bala-amypo <bala@amypo.in>
2025-12-18 16:36:04 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/AnalysisLogServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.AnalysisLog;
+import com.example.demo.repository.AnalysisLogRepository;
+import com.example.demo.service.AnalysisLogService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AnalysisLogServiceImpl implements AnalysisLogService {
+
+    private final AnalysisLogRepository analysisLogRepository;
+
+    public AnalysisLogServiceImpl(AnalysisLogRepository analysisLogRepository) {
+        this.analysisLogRepository = analysisLogRepository;
+    }
+
+    @Override
+    public AnalysisLog addLog(Long zoneId, String message) {
+
+        AnalysisLog log = new AnalysisLog();
+        log.setMessage(message);
+
+        return analysisLogRepository.save(log);
+    }
+
+    @Override
+    public List<AnalysisLog> getLogsByZone(Long zoneId) {
+        return analysisLogRepository.findAll();
+    }
+}
