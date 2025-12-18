a3bb7d89d7c5788b6880559c6dd6514cfd46489e
bala-amypo <bala@amypo.in>
2025-12-18 16:32:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/AnalysisLogRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.AnalysisLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnalysisLogRepository
+        extends JpaRepository<AnalysisLog, Long> {
+}
