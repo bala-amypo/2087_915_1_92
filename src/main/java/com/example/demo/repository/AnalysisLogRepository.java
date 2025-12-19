04d01bcc2517112147602b2c757df2866f030585
bala-amypo <bala@amypo.in>
2025-12-19 16:22:00 +0000

    User commit

--- src/main/java/com/example/demo/repository/AnalysisLogRepository.java
+++ src/main/java/com/example/demo/repository/AnalysisLogRepository.java
@@ -3,6 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.model.AnalysisLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnalysisLogRepository
-        extends JpaRepository<AnalysisLog, Long> {
+import java.util.List;
+
+public interface AnalysisLogRepository extends JpaRepository<AnalysisLog, Long> {
+
+    List<AnalysisLog> findByZone_Id(Long zoneId);
 }
