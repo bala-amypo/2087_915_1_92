118817ce040e86779222b53c2494776458e86d3f
bala-amypo <bala@amypo.in>
2025-12-19 09:20:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/HotspotZoneController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.HotspotZone;
+import com.example.demo.service.HotspotZoneService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/zones")
+public class HotspotZoneController {
+
+    private final HotspotZoneService service;
+
+    public HotspotZoneController(HotspotZoneService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public HotspotZone add(@RequestBody HotspotZone zone) {
+        return service.addZone(zone);
+    }
+
+    @GetMapping
+    public List<HotspotZone> getAll() {
+        return service.getAllZones();
+    }
+}
