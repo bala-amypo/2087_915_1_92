0c11d7a7c8921c6406106a6c437ebf375b880f93
bala-amypo <bala@amypo.in>
2025-12-19 06:08:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/CrimeReportController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.CrimeReport;
+import com.example.demo.service.CrimeReportService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reports")
+public class CrimeReportController {
+
+    private final CrimeReportService service;
+
+    public CrimeReportController(CrimeReportService service) {
+        this.service = service;
+    }
+    @PostMapping
+    public ResponseEntity<CrimeReport> add(
+            @RequestBody CrimeReport report) {
+
+        CrimeReport saved = service.addReport(report);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<CrimeReport>> getAll() {
+
+        List<CrimeReport> reports = service.getAllReports();
+        return new ResponseEntity<>(reports, HttpStatus.OK);
+    }
+}
+
+
---
+++ src/main/java/com/example/demo/repository/CrimeReportRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.CrimeReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CrimeReportRepository
+        extends JpaRepository<CrimeReport, Long> {
+}
+
+
