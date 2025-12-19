550e8d74251474f95ef20845296b1fcac6ba186b
bala-amypo <bala@amypo.in>
2025-12-19 09:35:49 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/HotspotZoneServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/HotspotZoneServiceImpl.java
@@ -3,37 +3,24 @@ package com.example.demo.service.impl;
 import com.example.demo.model.HotspotZone;
 import com.example.demo.repository.HotspotZoneRepository;
 import com.example.demo.service.HotspotZoneService;
-import com.example.demo.util.CoordinateValidator;
 
 import java.util.List;
 
 public class HotspotZoneServiceImpl implements HotspotZoneService {
 
-    private final HotspotZoneRepository zoneRepository;
+    private final HotspotZoneRepository repo;
 
-    public HotspotZoneServiceImpl(HotspotZoneRepository zoneRepository) {
-        this.zoneRepository = zoneRepository;
+    public HotspotZoneServiceImpl(HotspotZoneRepository repo) {
+        this.repo = repo;
     }
 
     @Override
     public HotspotZone addZone(HotspotZone zone) {
-
-        if (zoneRepository.findByZoneName(zone.getZoneName()) != null) {
-            throw new RuntimeException("Zone name already exists");
-        }
-        if (!CoordinateValidator.isValidLatitude(zone.getCenterLat())) {
-            throw new RuntimeException("Invalid latitude");
-        }
-        if (!CoordinateValidator.isValidLongitude(zone.getCenterLong())) {
-            throw new RuntimeException("Invalid longitude");
-        }
-
-        zone.setSeverityLevel("LOW");
-        return zoneRepository.save(zone);
+        return repo.save(zone);
     }
 
     @Override
     public List<HotspotZone> getAllZones() {
-        return zoneRepository.findAll();
+        return repo.findAll();
     }
 }
