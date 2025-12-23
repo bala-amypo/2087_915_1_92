c2835474626139e7646d62f784f271aacb05111a
bala-amypo <bala@amypo.in>
2025-12-23 10:11:31 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AnalysisLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AnalysisLogServiceImpl.java
@@ -1,13 +1,21 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.repository.*;
+import com.example.demo.repository.AnalysisLogRepository;
+import com.example.demo.repository.HotspotZoneRepository;
+import com.example.demo.service.AnalysisLogService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AnalysisLogServiceImpl {
+public class AnalysisLogServiceImpl implements AnalysisLogService {
+
+    private final AnalysisLogRepository logRepo;
+    private final HotspotZoneRepository zoneRepo;
 
     public AnalysisLogServiceImpl(
             AnalysisLogRepository logRepo,
             HotspotZoneRepository zoneRepo) {
+
+        this.logRepo = logRepo;
+        this.zoneRepo = zoneRepo;
     }
 }
