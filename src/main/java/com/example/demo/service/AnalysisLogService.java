a165e53ba4cca18bec292a57a9665418ee782926
bala-amypo <bala@amypo.in>
2025-12-23 10:14:32 +0000

    User commit

--- src/main/java/com/example/demo/service/AnalysisLogService.java
+++ src/main/java/com/example/demo/service/AnalysisLogService.java
@@ -5,7 +5,5 @@ import java.util.List;
 
 public interface AnalysisLogService {
 
-    AnalysisLog addLog(Long zoneId, String message);
-
     List<AnalysisLog> getLogsByZone(Long zoneId);
 }
--- src/main/java/com/example/demo/service/impl/AnalysisLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AnalysisLogServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.AnalysisLog;
 import com.example.demo.repository.AnalysisLogRepository;
 import com.example.demo.repository.HotspotZoneRepository;
 import com.example.demo.service.AnalysisLogService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AnalysisLogServiceImpl implements AnalysisLogService {
@@ -17,5 +20,11 @@ public class AnalysisLogServiceImpl implements AnalysisLogService {
 
         this.logRepo = logRepo;
         this.zoneRepo = zoneRepo;
+    }
+
+
+    @Override
+    public List<AnalysisLog> getLogsByZone(Long zoneId) {
+        return logRepo.findByZone_Id(zoneId);
     }
 }
